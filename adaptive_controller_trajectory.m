64aab1fdb75ff11b63fcd3aa38dc1ad67a11faa0
nicobonik <39326876+nicobonik@users.noreply.github.com>
2026-05-05 13:02:52 -0400

    add trajectory sim

---
+++ adaptive_controller_trajectory.m
@@ -0,0 +1,160 @@
+%% Setup robot
+clc, clear all, close all;
+
+%% Add Subfolder
+addpath("Communication_Code");
+addpath("generated_dynamics")
+
+%% Define robot
+robot = Robot();
+
+%% Define the type of low level control of the robot this is current mode
+robot.writeMode('c');
+
+%% Define sample time
+t_sample = 0.04;
+tfin = 10;
+t = 0:t_sample:tfin;
+
+tau_k = zeros(4, N);
+pi_k = zeros(16, N+1);
+dt = zeros(1, N);
+
+%% Factor from degres to rad
+factor_degre_to_rad = pi/180;
+factor_mA_to_A = 1/1000;
+factor_A_to_mA = 1000/1;
+
+%% Joint Positions
+q_real = zeros(4, length(t)+1);
+q_dot_real = zeros(4, length(t)+1);
+current_real = zeros(4, length(t)+1);
+
+
+%% Read Initial Conditions
+joint_readings = robot.getJointsReadings();
+q_real(:, 1) = joint_readings(1, :)*factor_degre_to_rad;
+q_dot_real(:, 1) = joint_readings(2, :)*factor_degre_to_rad;
+current_real(:, 1) = joint_readings(3, :)*factor_mA_to_A;
+
+%% Constants
+q1_desired = 0.5*ones(1, length(t));
+q2_desired = -0.35*ones(1, length(t));
+q3_desired =  0.3*ones(1, length(t));
+q4_desired =  0.15*ones(1, length(t));
+
+q_desired = [q1_desired; q2_desired; q3_desired; q4_desired];
+%% If you implement a full inverse dynamics controller you can define a desired velocity for each joint
+q_desired_dot = [0*q1_desired; 0*q2_desired; 0*q3_desired; 0*q4_desired];   
+
+%% System parameters
+R =load('Identification/identification_result.mat');
+p = [R.p(1:6); ...
+     R.x_opt_vec(1); R.x_opt_vec(2); R.x_opt_vec(3); R.x_opt_vec(4); ...
+     R.x_opt_vec(5); R.x_opt_vec(6); R.x_opt_vec(7); ...
+     R.x_opt_vec(8); R.x_opt_vec(9); R.x_opt_vec(10); ...
+     R.x_opt_vec(11); R.x_opt_vec(12); R.x_opt_vec(13); ...
+     R.x_opt_vec(14); R.x_opt_vec(15); R.x_opt_vec(16); ...
+     R.id_info.g];
+pf = [R.x_opt_vec(17); R.x_opt_vec(18); R.x_opt_vec(19); R.x_opt_vec(20)];
+
+
+%% Parameters
+% Joint natural frequencies (rad/s) - tune these
+omega = [5.0;    % joint 1 - base rotation   (large inertia ~0.011)
+         7.0;    % joint 2 - shoulder         (medium inertia ~0.046)
+         7.0;    % joint 3 - elbow            (small inertia ~0.008)
+         7.0];   % joint 4 - wrist            (tiny inertia ~0.0008)
+
+% Damping ratios per joint - tune these
+% 1.0 = critically damped, <1 = underdamped, >1 = overdamped
+zeta = [1.0;     % joint 1
+        1.2;     % joint 2
+        1.5;     % joint 3
+        1.0];    % joint 4
+
+% Inertia scaling at desired config
+M0 = M_fun(qd, p);
+m  = diag(M0);
+
+% Build gain matrices from per-joint parameters
+Kp     = diag([1, 1, 0.5, 0.2]);
+Kd     = diag([0.1, 0.1, 0.1, 0.1]);
+Gamma  = diag(0.05 * ones(16,1));  % adaptation
+
+%% Control Loop
+for k = 1:length(t) 
+    tic
+    %% Create Control Law Your Controller goes Here
+    [tau, pi_hat_next] = adaptive_controller(p, q_real(:, k), q_desired, q_dot_real(:, k), q_desired_dot, [0;0;0;0], pi_k(:,k), Kp, Kd, Gamma, t_sample);
+    tau_k(:, k) = tau;
+    pi_k(:, k+1) = pi_hat_next;
+    torques = [tau_k(1, k), tau_k(2, k), tau_k(3, k), tau_k(4, k)];
+    %% This is the mapping to Amperes
+    current = torque_to_current(torques);
+
+    %% This is the mapping to mA
+    current_mA = current*factor_A_to_mA;
+
+    robot.writeCurrents(current_mA);
+
+    while toc < t_sample
+    end
+    %% Sample time
+    dt(k) = toc;
+
+    %% Update measurements
+    joint_readings = robot.getJointsReadings();
+    q_real(:, k+1) = joint_readings(1, :)*factor_degre_to_rad;
+    q_dot_real(:, k+1) = joint_readings(2, :)*factor_degre_to_rad;
+    current_real(:, k+1) = joint_readings(3, :)*factor_mA_to_A;
+
+end
+
+%% Save data histories at sample k
+q_real_k = q_real(:, 1:length(t));
+q_dot_real_k = q_dot_real(:, 1:length(t));
+current_real_k = current_real(:, 1:length(t));
+
+%% Final Values
+tau = [0,0,0,0];
+current = tau;
+robot.writeCurrents(current); % Write joints to zero position
+disp("Movement Complete")
+
+figure(1)
+q_desired = [q1_desired; q2_desired; q3_desired; q4_desired];
+tau_all = tau_k;
+
+figure(1)
+for i = 1:4
+    subplot(2,2,i)
+    plot(t, q_desired(i,:), 'r.')
+    hold on
+    plot(t, q_real(i,1:length(t)), 'b.')
+    xlabel('Time [s]')
+    ylabel(['q', num2str(i), ' [rad]'])
+    title(['Joint ', num2str(i), ' Angle'])
+    legend('Desired', 'Current', 'Location', 'best')
+    grid on
+end
+
+figure(2)
+for i = 1:4
+    subplot(2,2,i)
+    plot(t, q_dot_real(i,1:length(t)), 'k.')
+    xlabel('Time [s]')
+    ylabel(['dq', num2str(i), ' [rad/s]'])
+    title(['Joint ', num2str(i), ' Velocity'])
+    grid on
+end
+
+figure(3)
+for i = 1:4
+    subplot(2,2,i)
+    plot(t, tau_all(i,:), 'm.')
+    xlabel('Time [s]')
+    ylabel(['tau', num2str(i), ' [Nm]'])
+    title(['Joint ', num2str(i), ' Control Torque'])
+    grid on
+end
